e5462c107564427fb5cae121191b25ed2411f318
Leonardo Prietsch Oliveira <leo.prietsch@gmail.com>
2022-01-26 09:05:56 -0300

    feat: heading options only for tag prop

--- components/Title.tsx
+++ components/Title.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import appConfig from '../styles.json';
 
 interface HeadingProps extends React.Attributes {
-  tag: string;
+  tag: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   children: string;
 }
 
