807131ef5f9c67d0d32f380628bb44ae68997f2b
Leonardo Prietsch Oliveira <leo.prietsch@gmail.com>
2022-01-26 09:02:59 -0300

    feat: title component for custom heading

---
+++ components/Title.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import appConfig from '../styles.json';
+
+interface HeadingProps extends React.Attributes {
+  tag: string;
+  children: string;
+}
+
+const Heading: React.FC<HeadingProps> = function Heading(props: HeadingProps) {
+  const { tag, children } = props;
+  return React.createElement(tag, null, children);
+};
+
+function Title(props: HeadingProps) {
+  const { tag, children } = props;
+
+  return (
+    <>
+      <Heading tag={tag}>{children}</Heading>
+      <style jsx>
+        {`
+          h1 {
+            color: ${appConfig.theme.colors.neutrals['000']};
+            font-size: 24px;
+            font-weight: 600;
+          }
+        `}
+      </style>
+    </>
+  );
+}
+
+export default Title;
