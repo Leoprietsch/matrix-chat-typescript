d7c27ae0733f75814116c41f832e10031445f2aa
Leonardo Prietsch Oliveira <leo.prietsch@gmail.com>
2022-01-31 07:24:29 -0300

    fix: remove semi colon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,6 @@ function App({ Component, pageProps }: AppProps) {
       <GlobalStyle />
       {/* eslint-disable-next-line react/jsx-props-no-spreading */}
       <Component {...pageProps} />
-      ;
     </>
   );
 }
