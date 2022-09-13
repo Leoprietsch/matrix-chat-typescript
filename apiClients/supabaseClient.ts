2b3040d4b50e7009aecb7eab29ca8560d1ae3a22
leoprietsch <leo.prietsch@gmail.com>
2022-09-12 21:10:37 -0300

    chore: remove old expired key

--- apiClients/supabaseClient.ts
+++ apiClients/supabaseClient.ts
@@ -1,7 +1,7 @@
-import { createClient, SupabaseRealtimePayload } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js';
 import Message from '../entities/Message';
 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM3Mjk5NywiZXhwIjoxOTU4OTQ4OTk3fQ.3kJmcXVd45b06taP0gvHPvG5a2Zpd1hGzO4FYTRCfGc';
+const SUPABASE_ANON_KEY = 'SUPABASE_ANON_KEY';
 const URL = 'https://vwtnjatwggxihvrnntha.supabase.co';
 const supabaseClient = createClient(URL, SUPABASE_ANON_KEY);
 
